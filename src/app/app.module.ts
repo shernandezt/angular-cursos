2734aa1ff3519a352fa97282f3c0e0ec6cd520fa
shernandezt <167793623+shernandezt@users.noreply.github.com>
2024-04-22 11:09:55 -0600

    sdffg

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { BrowserModule } from "@angular/platform-browser";
+import { HeroesModule } from "./heroes/heroes.module";
+import { ContadorModule } from "./contador/contador.module";
+
+@NgModule({
+    declarations: [
+        AppComponent,
+    ],
+    imports:[
+        BrowserModule,
+        HeroesModule,
+        ContadorModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+
+export class AppModule {}
